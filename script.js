e655c74b547b5713569ff11005b78860c7af4ed2
Filipe <filipe.b.braga@gmail.com>
2021-12-19 20:19:11 -0300

    add itens to cartlist

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function createProductItemElement({ sku, name, image }) {
 
 const addProductSection = async () => {
   const productElementAll = await fetchProducts('computador');
-  console.log(productElementAll);
   productElementAll.forEach(({ id, title, thumbnail }) => {
     const productElement = createProductItemElement({
       sku: id,
@@ -46,14 +45,30 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement(sku, name, salePrice) {
   const li = document.createElement('li');
+  const itemList = document.querySelector('.cart__items');
+
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  itemList.appendChild(li);
   return li;
+}
+
+function addItemClickListner() {
+  const btns = document.querySelectorAll('.item__add');
+  btns.forEach((btn) => {
+    btn.addEventListener('click', async () => {
+      const skuId = await getSkuFromProductItem(btn.parentNode);
+      const { id: sku1, title: name2, base_price: salePrice3 } = await fetchItem(skuId);
+      console.log(sku1, name2, salePrice3);
+      createCartItemElement(sku1, name2, salePrice3);
+    });
+ });
 }
 
 window.onload = async () => {
   await addProductSection();
+  await addItemClickListner();
 };
